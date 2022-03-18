893e43b26089a4eb9c826c041cc6c177fceac5bb
nvxd8c <nvxd8c@gmail.com>
2022-03-18 21:37:07 +0200

    Add setting cookies on the fly

--- commands.py
+++ commands.py
@@ -1,6 +1,8 @@
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler
 
+from handlers.tiktok_handler import TTHandler
+
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
@@ -10,7 +12,18 @@ def help(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="There's no help for you, bitch!")
 
 
+def cookie(update: Update, context: CallbackContext):
+    if text := update.message.text:
+        cookie = text.split('/cookie')[-1].strip()
+        TTHandler.set_cookie(cookie)
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Cookies are set")
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="ERROR")
+
+
 HANDLERS = [
     CommandHandler('start', start),
     CommandHandler('help', help),
+    CommandHandler('cookie', cookie),
+
 ]
