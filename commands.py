5250c24ccb66681c0bdeb15bc0a9b56b63a2c9a7
nvxd8c <nvxd8c@gmail.com>
2022-03-23 22:17:08 +0200

    Add remove_origin command. Setting cookies only for admins

--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
+import os
+
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler
 
@@ -9,10 +11,21 @@ def start(update: Update, context: CallbackContext):
 
 
 def help(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="There's no help for you, bitch!")
+    text = \
+        """
+/start: to start bot
+/help: this help
+/cookie HEADER: To set global cookies. Only admins can do
+/remove_origin True/False: To remove origin message. Admin access required
+        """
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 
 def cookie(update: Update, context: CallbackContext):
+    if update.effective_user in [admin.user for admin in update.effective_chat.get_administrators()] is False:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Only admins can set cookies")
+        return
+
     if text := update.message.text:
         cookie = text.split('/cookie')[-1].strip()
         TTHandler.set_cookie(cookie)
@@ -21,9 +34,16 @@ def cookie(update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=update.effective_chat.id, text="ERROR")
 
 
+def remove_origin(update: Update, context: CallbackContext):
+    text = update.message.text.split('/remove_origin', 1)
+    if len(text) == 2:
+        os.environ['REMOVE_ORIGIN'] = 'True' if text[1].strip() == "True" else 'False'
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Remove origin is {os.environ['REMOVE_ORIGIN']}")
+
+
 HANDLERS = [
     CommandHandler('start', start),
     CommandHandler('help', help),
     CommandHandler('cookie', cookie),
-
+    CommandHandler('remove_origin', remove_origin),
 ]
--- handlers/msg_handler.py
+++ handlers/msg_handler.py
@@ -1,6 +1,7 @@
+import os
 import re
 
-from telegram import Update, ParseMode
+from telegram import Update, ParseMode, error
 from telegram.ext import MessageHandler, Filters, CallbackContext
 
 from handlers.tiktok_handler import TTHandler
@@ -20,7 +21,17 @@ def msg_handler(update: Update, context: CallbackContext):
         if video_link := tt.get_video_link():
             formatted_tt_link = f"<a href='{video_link}'>Video link</a>"
             text = input_text.replace(link, formatted_tt_link)
+            if not update.effective_message.from_user.is_bot:
+                text = f">{update.effective_message.from_user.username}: \n{text}"
+
             context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.HTML)
+            try:
+
+                if os.environ.get('REMOVE_ORIGIN', False) == 'True':
+                    update.message.delete()
+            except error.BadRequest as e:
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text="Can't remove origin message. Check permissions", parse_mode=ParseMode.HTML)
             return
 
         output_text = "Could not parse link"
